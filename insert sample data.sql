4cf79052ed0e99b8219d5c66fe9388ca206ad5ed
christopher decker <decker389@aol.com>
2021-03-06 22:36:48 -0700

    added sample data queries for tables students and courses

--- insert sample data.sql
+++ insert sample data.sql
@@ -1,5 +1,5 @@
 USE school_project_schema;
--- script for creating data
+-- script for creating data on students table
 
 INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
 VALUES ("Jordan", "hudgens", "hudens@fake.com", 0);
@@ -9,6 +9,72 @@ VALUES("bob", "decker", "bob@bob.com", 0);
 
 INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
 VALUES ("Brittany", "Boring", "boring@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "Smith", "smith@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Triston", "Smith", "tristonS@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "sample", "sample@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Triston", "sample", "tristonsample@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "Hello", "helloagain@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "bye", "goodbye@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "Cheesecake", "cheesecake@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "Boring", "boring@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Chris", "Pizza", "pizza@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("destorying", "pizza", "destroyingpizza@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("making", "pizza", "custompizza@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Chris", "pie", "ihatepie@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "pie", "sisterlovespie@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("fake", "Boring", "fake@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "fake", "lotsoffakse@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "chris", "brittanychris@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "Triston", "brittanytriston@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "Borred", "theborred@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "Bored", "thebrittany@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("triston", "cheesecake", "tristoncheesecake@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("triston", "pizza", "tristonpizza@fake.com", 0);
+
+INSERT INTO students(students_first_name, students_last_name, students_email, students_status)
+VALUES ("Brittany", "pizza", "brittanypizza@fake.com", 0);
 
 SELECT *
 FROM students;
---
+++ sample data for courses table.sql
@@ -0,0 +1,19 @@
+USE school_project_schema;
+
+INSERT INTO courses(courses_name, courses_subject)
+VALUES ("Manifest Destiny", "History");
+
+INSERT INTO courses(courses_name, courses_subject)
+VALUES ("High Hopes", "Music");
+
+INSERT INTO courses(courses_name, courses_subject)
+VALUES ("Algrebra", "Math");
+
+INSERT INTO courses(courses_name, courses_subject)
+VALUES ("Earth Science", "Science");
+
+INSERT INTO courses(courses_name, courses_subject)
+VALUES ("Pyshics", "Science");
+
+SELECT *
+FROM courses;
